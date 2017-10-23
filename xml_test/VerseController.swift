7af864e4ad3b998a8fa668f2286443ba28dff1b7
Simon Brüchner <powtac@gmx.de>
2017-10-24 00:49:28 +0200

    Ausnahmen und weitere optionale Textteile berücksichtigt.

--- xml_test/VerseController.swift
+++ xml_test/VerseController.swift
@@ -21,29 +21,28 @@ class VerseController: UIViewController {
                 config.shouldProcessLazily = true
             }.parse(text)
             
-            
-            let textView = UITextView(frame: CGRect(x: 0, y: 0, width: 250.0, height: 100.0))
-            // textView.isSelectable = true
-            textView.frame.size.width  = UIScreen.main.bounds.width
-            textView.frame.size.height = UIScreen.main.bounds.height
+            let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height + self.navigationController!.navigationBar.frame.height
+            let textView = UITextView(frame: CGRect(x: 0, y: barHeight, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - barHeight))
             textView.isEditable = false
             view.addSubview(textView)
             
+            // https://stackoverflow.com/a/18932245/22470
+            self.automaticallyAdjustsScrollViewInsets = false
             
             let textViewText    = textView.attributedText.mutableCopy() as! NSMutableAttributedString
-            let us1Font         = UIFont.preferredFont(forTextStyle: .title1) // UIFont(name: "Arial", size: 20)!
+            let us1Font         = UIFont.preferredFont(forTextStyle: .title1)
             let versFont        = UIFont.preferredFont(forTextStyle: .body)
             let fussnoteFont    = UIFont.preferredFont(forTextStyle: .footnote)
 
-            var fussnoteParagraphStyle: NSMutableParagraphStyle = {
+            let fussnoteParagraphStyle: NSMutableParagraphStyle = {
                 let style = NSMutableParagraphStyle()
                 style.lineSpacing = 3.5
                 style.paragraphSpacingBefore = 0
                 style.paragraphSpacing = 10.5
                 return style
             }()
-
-            var us1ParagraphStyle: NSMutableParagraphStyle = {
+            
+            let us1ParagraphStyle: NSMutableParagraphStyle = {
                 let style = NSMutableParagraphStyle()
                 style.lineSpacing = 1
                 style.paragraphSpacingBefore = 33
@@ -54,33 +53,38 @@ class VerseController: UIViewController {
             // Verse ausgeben
             let elem2 = xml["Bibel"]["Buch"]["Kapitel"][self.Kapitel!-1] // Hier wird genau der Kapitel-Node im XML ausgewählt!
             for elem4 in elem2["Abschnitt"].all {
-
-                let us1 = NSAttributedString(string: elem4["Us1"].element!.text+"\n", attributes: [NSAttributedStringKey.font: us1Font, NSAttributedStringKey.paragraphStyle: us1ParagraphStyle])
-                textViewText.append(us1)
+                
+                if ((elem4["Us1"].element) != nil) {
+                    let us1 = NSAttributedString(string: elem4["Us1"].element!.text+"\n", attributes: [NSAttributedStringKey.font: us1Font, NSAttributedStringKey.paragraphStyle: us1ParagraphStyle])
+                    textViewText.append(us1)
+                }
 
                 for elem3 in elem4["Grundtext"]["Vers"].all {
                     let vers = NSAttributedString(string: elem3["Versziffer"].element!.text+" "+elem3["Verstext"].element!.text+"\n", attributes:[NSAttributedStringKey.font: versFont])
                     textViewText.append(vers)
                 }
                 
+                // Ausnahmen am Kapitelende wie "Euer Paulus"
+                if ((elem4["Ausnahme"].element) != nil) {
+                    let ausnahme = NSAttributedString(string: elem4["Ausnahme"].element!.text+"\n", attributes:[NSAttributedStringKey.font: versFont])
+                    textViewText.append(ausnahme)
+                }
+                
                 for elem3 in elem4["Fussnote"].all {
                     let fussnote = NSAttributedString(string: "\n"+elem3["Fussnotenummer"].element!.text+" "+elem3["Fussnotetext"].element!.text, attributes:[NSAttributedStringKey.font: fussnoteFont, NSAttributedStringKey.paragraphStyle: fussnoteParagraphStyle])
                     textViewText.append(fussnote)
-                    print(elem3["Fussnotetext"].element!.text)
+                    // print(elem3["Fussnotetext"].element!.text)
                 }
             }
             
-            
-            func enumerate(indexer: XMLIndexer) {
-                for child in indexer.children {
-                    print(child.element!.name)
-                    enumerate(indexer: child)
-                }
-            }
-            enumerate(indexer: xml["Bibel"]["Buch"]["Kapitel"][self.Kapitel!-1])
-            
-            
-            // TODO hier fehlen auch noch die Fußnoten in spezieller Formatierung
+// Debug
+//            func enumerate(indexer: XMLIndexer) {
+//                for child in indexer.children {
+//                    print(child.element!.name)
+//                    enumerate(indexer: child)
+//                }
+//            }
+//            enumerate(indexer: xml["Bibel"]["Buch"]["Kapitel"][self.Kapitel!-1])
             
             textView.attributedText = textViewText.copy() as! NSAttributedString
             

ae363f47ad8b729a402868ecd1551cf8115a1070
Simon Brüchner <powtac@gmx.de>
2017-10-24 01:27:01 +0200

    BundleID Hinweis

---
+++ BundleID-Hinweis.swift
@@ -0,0 +1,17 @@
+
+//307707.92Q57BFW94
+//
+//E2J655BKXJ.92Q57BFW94
+//
+//
+//
+//
+//Aus vorheriger App Datei:
+//
+//volxbibel_swift.app/volxbibel_swift
+//application-identifier: 92Q57BFW94.92Q57BFW94
+//get-task-allow: false
+//beta-reports-active: true
+//keychain-access-groups: ( "92Q57BFW94.92Q57BFW94" )
+//com.apple.developer.team-identifier: E2J655BKXJ
+
